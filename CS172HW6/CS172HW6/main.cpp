c5a2b5b4f2128a304ef1bc7decb828496f269e51
Heidi Wiebers <hwiebers19@hotmail.com>
2016-11-03 13:53:22 -0700

    started

---
+++ CS172HW6/CS172HW6/main.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//  CS172HW6
+//
+//  Created by Heidi Wiebers on 11/3/16.
+//  Copyright © 2016 Heidi Wiebers. All rights reserved.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <ctime>
+#include <cctype>
+#include <vector>
+using namespace std;
+
+//EX06_01 – Liang Programming Exercise 13.1: Create a text file
+//EX06_02 – Liang Programming Exercise 13.2: Count characters
+//EX06_03 – Liang Programming Exercise 13.5: Baby names
+//EX06_04 – Liang Programming Exercise 14.3: The Circle class
+//EX06_05 – Liang Programming Exercise 14.7: Math: The Complex class
+
+int main() {
+    //EX06_01 – Liang Programming Exercise 13.1: Create a text file
+    
+    
+    //EX06_02 – Liang Programming Exercise 13.2: Count characters
+    
+    
+    //EX06_03 – Liang Programming Exercise 13.5: Baby names
+    
+    
+    //EX06_04 – Liang Programming Exercise 14.3: The Circle class
+    
+    
+    //EX06_05 – Liang Programming Exercise 14.7: Math: The Complex class
+    
+    
+    
+    
+    return 0;
+}
